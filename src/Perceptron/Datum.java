0c5fdca87c09cacbb146e920e28aa79e108f0bcf
thanakorntr <thanakorn@nyu.edu>
2015-05-25 18:08:31 -0400

    Implemented simple perceptron

--- src/Perceptron/Datum.java
+++ src/Perceptron/Datum.java
@@ -4,4 +4,11 @@ package Perceptron;
  * Created by Thanakorn on 5/25/15.
  */
 public class Datum {
+
+    public double[] featureVector;
+    public double trueLabel;
+
+    public Datum() {
+
+    }
 }
